1b008ba28e31c98928ec2951a236eb70b30525e1
rzn-git <rznislam@gmail.com>
2024-10-07 07:59:26 +0600

    working

--- profiler.py
+++ profiler.py
@@ -1,8 +1,15 @@
+import os
 from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
+import openai 
 
+openai.api_key = os.getenv('OPENAI_API_KEY')
 
-if __name__ == "__main__"
+information = """
+    Elon Reeve Musk  has expressed views that have made him a polarizing figure.[5] He has been criticized for making unscientific and misleading statements, including COVID-19 misinformation, promoting right-wing conspiracy theories, and "endorsing an antisemitic theory"; he later apologized for the last of these.[6][5][7] His ownership of Twitter has been controversial because of the layoffs of large numbers of employees, an increase in hate speech, misinformation and disinformation posts on the website, and changes to Twitter Blue verification. 
+    """
+
+if __name__ == "__main__":
     print ("Hello Langchain")
 
 
@@ -13,8 +20,12 @@ if __name__ == "__main__"
         information-  {information} 
     """
 
-    summary_prompt_template = PromptTemplate(input_variables="information", template = summary_template)
+    summary_prompt_template = PromptTemplate(input_variables=   ["information"], template = summary_template)
 
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
 
-    chain = summary_prompt_template
+    chain = summary_prompt_template | llm
+
+    response = chain.invoke(input = {"information": information})
+
+    print (response)
---
+++ profiler_1.py
@@ -0,0 +1,37 @@
+import os
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+import openai
+
+# Set your API key here
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
+information = """
+    List of country - capital
+    France - Paris
+    Germany - Berlin
+    Bangladesh - Dhaka
+    South Africa - Pretoria
+    """
+
+if __name__ == "__main__":
+    
+    # Modify the template to ask a specific question
+    summary_template = """ 
+        From the following information, what is the capital of Bangladesh? {information}
+    """
+
+    summary_prompt_template = PromptTemplate(input_variables=["information"], template=summary_template)
+
+    # Define the language model
+    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+
+    # Create the chain (pipe the prompt template into the language model)
+    chain = summary_prompt_template | llm
+
+    # Invoke the chain with input
+    response = chain.invoke(input={"information": information})
+
+    # Print only the content
+    print(response.content)
+    
